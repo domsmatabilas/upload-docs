ebf99994436f0e7eb29b51101a2c8911fcaaa9dd
Bernard <matabilas.bernard@gmail.com>
2021-05-15 16:48:17 +0300

    angular

---
+++ upload-docs/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {AppComponent} from './app.component';
+const routes: Routes = [
+  {path:'', component: AppComponent},
+  {path:'login', component: AppComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
